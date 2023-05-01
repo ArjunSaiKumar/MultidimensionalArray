e10f40116e4215c58d8948c9202e33f5ac11460b
sai kumar <saiprakash837@outlook.com>
2023-05-02 00:17:42 +0530

    First push

---
+++ MultidimensionalArrayLibrary/TwoDArray.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultidimensionalArrayLibrary
+{
+    /// <summary>
+    /// This TwoDArray class will operate with multiple functionalites.
+    /// </summary>
+    public class TwoDArray
+    {
+        private int _rank;
+        public int RowLenght;
+        public int ColumnLenght;
+
+        public TwoDArray(Array _2DArray)
+        {
+            _rank = _2DArray.Rank;
+            RowLenght = _2DArray.GetLength(0);
+            if (_rank != 2)
+                throw new Exception("Invalid array, An array should be 2D form");
+
+            ColumnLenght = _2DArray.GetLength(1);
+
+            if (RowLenght != ColumnLenght)
+                throw new Exception("Invalid array, An array should be in cube form. (equal in both lenghts)");
+
+        }
+    }
+}
---
+++ MultidimensionalArrayTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using MultidimensionalArrayLibrary;
+
+namespace MultidimensionalArrayTest
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                TwoDArray twoDArray = new(new[,] { { 1, 4, 2 }, { 3, 6, 8 } });//, { 7, 5, 9 }
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+    }
+}
